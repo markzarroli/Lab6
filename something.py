c27fa15468576706e357ecab23a41c644a21732c
markzarroli <58492942+markzarroli@users.noreply.github.com>
2024-02-27 14:47:25 -0500

    Create something.py

---
+++ something.py
@@ -0,0 +1,13 @@
+def main():
+    # Taking input for name
+    name = input("Enter your name: ")
+
+    # Taking input for age
+    age = input("Enter your age: ")
+
+    # Printing name and age
+    print("Your name is:", name)
+    print("Your age is:", age)
+
+if __name__ == "__main__":
+    main()
